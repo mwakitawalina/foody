98c0cf4e8120e842d0387087f50f662810c91f47
Paul Ogolla <paulotieno2@gmail.com>
2023-11-23 11:42:26 +0300

    fix syntax errors

--- lib/screens/majorscreens/dashboard.dart
+++ lib/screens/majorscreens/dashboard.dart
@@ -9,42 +9,37 @@ class Dashboard extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            decoration: BoxDecoration(
-              image:DecorationImage(
-                image: AssetImage("assets/images/bg.jpg"),
-                fit: BoxFit.cover,
-                ) 
-            ),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/images/bg.jpg"),
+              fit: BoxFit.cover,
+            )),
           )
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-            BottomNavigationBarItem(
+        items: const [
+          BottomNavigationBarItem(
             icon: Icon(Icons.add_home),
             label: "home",
-            backgroundColor:const Color(0xFFf54748) ,
-            ),
-
-             BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "search",
-            backgroundColor: Colors.orangeAccent
-            ),
-
-             BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: "favorite",
-            backgroundColor: Colors.orangeAccent
-            ),
-
-             BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle,),
-            label: "account",
-            backgroundColor: Colors.orangeAccent
-            ),
+            backgroundColor: Color(0xFFf54748),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "search",
+              backgroundColor: Colors.orangeAccent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "favorite",
+              backgroundColor: Colors.orangeAccent),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+              ),
+              label: "account",
+              backgroundColor: Colors.orangeAccent),
         ],
-        ),
+      ),
     );
   }
 }
