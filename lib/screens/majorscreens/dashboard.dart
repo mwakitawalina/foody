09e331dcfc1f6014de81084b7116d98435726087
mwakitawalina <68387643+mwakitawalina@users.noreply.github.com>
2023-11-23 11:45:32 +0300

    Ft homescreen (#22)
    * updated landing page
    
    * Button and fonts
    
    * Latest updates on custom button
    
    * homescreen updated with a bottom navigation bar
    
    * fix syntax errors
    
    ---------
    
    Co-authored-by: Paul Ogolla <paulotieno2@gmail.com>

--- lib/screens/majorscreens/dashboard.dart
+++ lib/screens/majorscreens/dashboard.dart
@@ -5,6 +5,44 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/images/bg.jpg"),
+              fit: BoxFit.cover,
+            )),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_home),
+            label: "home",
+            backgroundColor: Color(0xFFf54748),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "search",
+              backgroundColor: Colors.orangeAccent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "favorite",
+              backgroundColor: Colors.orangeAccent),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+              ),
+              label: "account",
+              backgroundColor: Colors.orangeAccent),
+        ],
+      ),
+    );
+  }
+}
     return const Placeholder();
   }
 }
